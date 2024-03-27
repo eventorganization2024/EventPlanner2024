fd352f2a9da8128ef5183276f14cad499c6bdf4c
Haneen-Radad <160304061+Haneen-Radad@users.noreply.github.com>
2024-03-27 17:43:35 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,5 @@
 package org.example;
-import java.util.*;
 import java.util.Scanner;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
-import org.example.Functions;
-import java.io.*;
-import java.net.PasswordAuthentication;
 
 
 
@@ -25,39 +13,59 @@ public class Main {
 	        
 	    	
 	    	
-	         while(true){
-	            printing.printSomething("\n------ Welcome to Home Page ------"+"\n|                                |"+
-	                    "\n|          1. Sign up            |"+"\n|          2. Sign in            |"+
-	                    "\n|          3. Exit               |"+"\n|                                |"+
-	                    "\n----------------------------------\n"+"Enter your choice: ");
+	        while(true) {
+	        	printing.printSomething("""
+    		    ------ Welcome to Home Page ------
+    		    |                                |
+    		    |          1. Sign up            |
+    		    |          2. Sign in            |
+    		    |          3. Exit               |
+    		    |                                |
+    		    ----------------------------------
+    		    Enter your choice: 
+	        	""");
+
 	            while (!scanner.hasNextInt()) {
 	                printing.printSomething("Invalid input! Please enter a valid integer.");
 	                scanner.next();
 	            }
 	            
-	            functions.choice = scanner.nextInt();
-	           if(functions.choice ==1 ) 
-	           {   
-	        	   printing.printSomething("\n------ SINGUP PAGE ------"+"\n|                                |"+
-		                    "\n|          1. CUSTOMER            |"+"\n|          2. PROVIDER           |"+
-		                    "\n|          3. Exit                |"+"\n|                                |"+
-		                    "\n----------------------------------\n"+"Enter your choice: ");
-	        	   
-	        	   while (!scanner.hasNextInt()) {
-		                printing.printSomething("Invalid input! Please enter a valid integer.");
-		                scanner.next();
-		            }
-		            
-		            functions.choice2 = scanner.nextInt();
-	        	   
-	        	   if (functions.choice2==1) { functions.customerSignUp();}
-	        	   else if (functions.choice2==2) {functions.providerSignUp();}
-	        	   else  if(functions.choice2 == 3) functions.choice =1; 
-	           }
-	           else  if(functions.choice == 2 )functions.signInFunction();
-	           else  if(functions.choice == 3) System.exit(0);
-	            else printing.printSomething("Invalid choice! Please enter a valid choice.");
+	            Functions.choice = scanner.nextInt();
+	            if (Functions.choice == 1) {   
+	            	printing.printSomething("""
+        		    ----------- SINGUP PAGE ----------
+        		    |                                |
+        		    |          1. CUSTOMER           |
+        		    |          2. PROVIDER           |
+        		    |          3. Exit               |
+        		    |                                |
+        		    ----------------------------------
+        		    Enter your choice: 
+        		    """);
+
+	                while (!scanner.hasNextInt()) {
+	                    printing.printSomething("Invalid input! Please enter a valid integer.");
+	                    scanner.next();
+	                }
+	                
+	                functions.choice2 = scanner.nextInt();
+	                
+	                if (functions.choice2 == 1) {
+	                    functions.customerSignUp();
+	                } else if (functions.choice2 == 2) {
+	                    functions.providerSignUp();
+	                } else if (functions.choice2 == 3) {
+	                    break; // Exit the loop
+	                }
+	            } else if (Functions.choice == 2) {
+	                Functions.signInFunction();
+	            } else if (Functions.choice == 3) {
+	                System.exit(0);
+	            } else {
+	                printing.printSomething("Invalid choice! Please enter a valid choice.");
+	            }
 	        }
+
 	    }
 }
 
