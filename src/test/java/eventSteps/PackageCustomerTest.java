ef5923d251ccf0b9f5ffac004c8d4fc2dfc18ac4
Jullnar-Hajeh <160051567+Jullnar-Hajeh@users.noreply.github.com>
2024-03-28 02:15:59 +0200

    Update and rename packagecusttest.java to PackageCustomerTest.java

--- src/test/java/eventSteps/PackageCustomerTest.java
+++ src/test/java/eventSteps/PackageCustomerTest.java
@@ -10,7 +10,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class packagecusttest {
+public class PackageCustomerTest {
     private Admin admin;
     private Paackage selectedPackage;
     private boolean packageAddedSuccessfully;
