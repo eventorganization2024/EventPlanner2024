02360952a513afe56f869ca4d1e9e582de59b208
yamamah ashayer <159881188+yamamahashayer@users.noreply.github.com>
2024-03-28 04:59:15 -0700

    Update and rename UserRegSteps.java to UserRegTest.java

--- src/test/java/eventSteps/UserRegTest.java
+++ src/test/java/eventSteps/UserRegTest.java
@@ -8,16 +8,17 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class UserRegSteps {
+public class UserRegTest {
 	
 	
 	   User u = new User();
-	    Logger logger = Logger.getLogger(UserRegSteps.class.getName());
+	    Logger logger = Logger.getLogger(UserRegTest.class.getName());
 
 
-	    ///
-	 @Given("customers")
-	  
+	    @Given("customers")
+	    public void customersStep() {
+	        // Implementation of the step goes here
+	    }
 	
 	@Given("there is a User with User ID {string} ,NAME {string} , PASSWORD {string} , phone {string}, address {string}")
 	public void thereIsAUserWithUserIDNAMEPASSWORDPhoneAddress(String string, String string2, String string3, String string4, String string5) {
