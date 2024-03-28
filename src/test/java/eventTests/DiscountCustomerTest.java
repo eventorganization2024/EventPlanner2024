dd894b72b136bffe613f1bea21bd690ca3d96d67
yamamah ashayer <159881188+yamamahashayer@users.noreply.github.com>
2024-03-28 06:18:10 -0700

    Update and rename Discountcustomertest.java to DiscountCustomerTest.java

--- src/test/java/eventTests/DiscountCustomerTest.java
+++ src/test/java/eventTests/DiscountCustomerTest.java
@@ -6,7 +6,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class Discountcustomertest {
+public class DiscountCustomerTest {
 	Discount d= new Discount ();
 	boolean expired;
 double oldprice ;
