3e7157c4c35d003d01e3e988830df523cd202589
yamamah ashayer <159881188+yamamahashayer@users.noreply.github.com>
2024-03-28 05:01:54 -0700

    Update and rename venueAvailability.java to venueAvailabilityTest.java

--- src/test/java/eventSteps/venueAvailabilityTest.java
+++ src/test/java/eventSteps/venueAvailabilityTest.java
@@ -6,7 +6,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class venueAvailability
+public class venueAvailabilityTest
 {
 	 private Venue v= new Venue();
 	
