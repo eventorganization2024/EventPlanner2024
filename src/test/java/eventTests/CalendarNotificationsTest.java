49089da2bdfcc829e9240a6831c7b7552f798db7
Haneen-Radad <160304061+Haneen-Radad@users.noreply.github.com>
2024-04-01 22:35:43 +0200

    Add files via upload

---
+++ src/test/java/eventTests/CalendarNotificationsTest.java
@@ -0,0 +1,49 @@
+package eventSteps;
+
+import org.example.Calendar;
+import org.example.Functions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
+public class CalendarNotificationsTest {
+	
+    Functions functions = new Functions();
+
+    
+    @Given("the user is logged in")
+    public void theUserIsLoggedIn() {
+    }
+
+    @When("the user opens the calendar")
+    public void theUserOpensTheCalendar() {
+    }
+
+    @Then("the user should see a list of upcoming events")
+    public void theUserShouldSeeAListOfUpcomingEvents() {
+    	
+    	 Calendar calendar = Functions.loadEventsForCustomerInCalendar("11");
+     	if (calendar != null) {
+     	 Functions.displayAllCustomerEvents(calendar);
+     	 } 
+    }
+    
+
+    @Given("there is an upcoming event")
+    public void thereIsAnUpcomingEvent() {
+    	//assume there is upcoming event
+    }
+    @When("the notification system checks for upcoming events")
+    public void theNotificationSystemChecksForUpcomingEvents() {
+    	
+    }
+    @Then("the user should receive a notification for the event")
+    public void theUserShouldReceiveANotificationForTheEvent() {
+        functions.startApproachingUpcomingEvents();
+
+    }
+
+
+
+}
