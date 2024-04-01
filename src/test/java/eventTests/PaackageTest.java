7a0d90553ed671282024ddc947d5d17a97f0b038
jumanasaif <160027530+jumanasaif@users.noreply.github.com>
2024-04-01 04:38:48 +0300

    Update PaackageTest.java

--- src/test/java/eventTests/PaackageTest.java
+++ src/test/java/eventTests/PaackageTest.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.io.PrintWriter;
-import java.lang.reflect.InvocationTargetException;
+
 import java.lang.reflect.Method;
 
 import org.example.Paackage;
