120d73b766c6ad403b66e14fab47bf203d58844f
Haneen-Radad <160304061+Haneen-Radad@users.noreply.github.com>
2024-04-01 22:03:28 +0200

    Update Functions.java

--- src/main/java/org/example/Functions.java
+++ src/main/java/org/example/Functions.java
@@ -26,8 +26,6 @@ import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-
-import java.util.stream.Collectors;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
