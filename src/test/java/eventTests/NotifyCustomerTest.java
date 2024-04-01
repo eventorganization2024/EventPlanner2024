ab9816ef9db62183969027eb8849c04817e3be36
jumanasaif <160027530+jumanasaif@users.noreply.github.com>
2024-04-01 18:58:59 +0300

    Update NotifyCustomerTest.java

--- src/test/java/eventTests/NotifyCustomerTest.java
+++ src/test/java/eventTests/NotifyCustomerTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
