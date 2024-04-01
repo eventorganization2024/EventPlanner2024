f37285a938223d368fa040b69dd079726b493c87
jumanasaif <160027530+jumanasaif@users.noreply.github.com>
2024-04-01 07:25:34 +0300

    Update CustomerActionTest.java

--- src/test/java/eventTests/CustomerActionTest.java
+++ src/test/java/eventTests/CustomerActionTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
